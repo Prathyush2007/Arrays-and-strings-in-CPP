a1f9e78c6dfc6d3887d5a49701e95abfcaf12be0
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:14:45 +0530

    Create Taking string as input and display it.cpp

---
+++ Taking string as input and display it.cpp
@@ -0,0 +1,12 @@
+//Name : Prathyush Nimmagadda
+//PRN : 24070123064
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+int main() {
+    char str[50];
+    cout << "Enter a string: ";
+    cin >> str;  
+    cout <<"You entered:"<< str << endl;
+    return 0;
+}
